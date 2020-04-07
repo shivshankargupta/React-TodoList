b79f3836db8ffd103b908131ad3f7185dd1447ef
Shiv Shankar <guptashiv199701@gmail.com>
2020-04-07 19:24:38 +0530

    React-TodoList

--- src/App.js
+++ src/App.js
@@ -1,26 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import TodoList from "./Component/TodoList";
+import "./App.css";
 
-function App() {
+class App extends Component{
+ 
+  state = {
+    todo: '',
+    list: []
+  }
+
+  handleChange (e){
+  this.setState({
+   todo: e.target.value
+  });
+}
+
+addTheTodo(){
+  const {todo,list} = this.state
+  if(todo === "") return
+
+  const dataToAdd = {
+    done: false,
+    name: todo
+  }
+
+  list.push(dataToAdd)
+  this.setState({
+    list,
+    todo: ""
+  })
+}
+doneTheTodo(e){
+   const {value,checked} = e.target
+   const {list} = this.state
+   const i = list.findIndex(f => f.name === value)
+   if( i === -1) return
+   list [i].done = checked
+   this.setState({
+     list
+   })
+}
+renderList(){
+  const  {list} = this.state
+  const data = list.map((d,i)=>{
+    return (
+     
+      <Mylist key={i} onChange={this.doneTheTodo.bind(this)} done={d.done} name={d.name} />
+    )
+  } )
+   return data
+  
+}
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="jumbotron mt-5">
+        <div className="form-group">
+          <label htmlFor="User" id="User"> Name</label>
+      <input type="text"  className="from-control w-100" id="user" onChange={this.handleChange.bind(this)} value={this.state.todo} />
+
+        </div>
+       
+      <button className="btn btn-primary" onClick={this.addTheTodo.bind(this)}>Add</button>
+
+      </div>
+      {this.renderList()}
+   
     </div>
   );
 }
+}
+
+const Mylist = (props) =>{
+  let name
+  let myClass
+  if(props.done){
+    name = <strike >props.name</strike>
+    myClass = "alert alert-success"
+  }else{
+    name = props.name
+    myClass = "alert alert-info"
+  }
+  return(
+    <React.Fragment>
+      <div className={myClass}>
+
+     
+      <div className="checkbox ">
+  <label><input  value={props.name} onChange={props.onChange} type="checkbox" checked={props.done} />{name}</label>
+  </div>
+      </div>
+    </React.Fragment>
+  )
+}
+
+
+
 
 export default App;
